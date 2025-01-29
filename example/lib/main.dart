e954b68cdef0728155d02be3bc13c204095b2121
Gusakovsky, Sergey <sgusakovsky@qulix.com>
2025-01-29 14:06:40 +0300

    Code refactoring

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,64 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _jitsiMeetPlugin = JitsiMeet();
+
+  final _jitsiMeetListener = JitsiMeetEventListener(
+    conferenceJoined: (url) {
+      debugPrint("conferenceJoined: url: $url");
+    },
+    conferenceTerminated: (url, error) {
+      debugPrint("conferenceTerminated: url: $url, error: $error");
+    },
+    conferenceWillJoin: (url) {
+      debugPrint("conferenceWillJoin: url: $url");
+    },
+    participantJoined: (email, name, role, participantId) {
+      debugPrint(
+        "participantJoined: email: $email, name: $name, role: $role, "
+            "participantId: $participantId",
+      );
+    },
+    participantLeft: (participantId) {
+      debugPrint("participantLeft: participantId: $participantId");
+    },
+    audioMutedChanged: (muted) {
+      debugPrint("audioMutedChanged: isMuted: $muted");
+    },
+    videoMutedChanged: (muted) {
+      debugPrint("videoMutedChanged: isMuted: $muted");
+    },
+    endpointTextMessageReceived: (senderId, message) {
+      debugPrint(
+          "endpointTextMessageReceived: senderId: $senderId, message: $message");
+    },
+    screenShareToggled: (participantId, sharing) {
+      debugPrint(
+        "screenShareToggled: participantId: $participantId, "
+            "isSharing: $sharing",
+      );
+    },
+    chatMessageReceived: (senderId, message, isPrivate, timestamp) {
+      debugPrint(
+        "chatMessageReceived: senderId: $senderId, message: $message, "
+            "isPrivate: $isPrivate, timestamp: $timestamp",
+      );
+    },
+    chatToggled: (isOpen) => debugPrint("chatToggled: isOpen: $isOpen"),
+    participantsInfoRetrieved: (participantsInfo) {
+      debugPrint(
+          "participantsInfoRetrieved: participantsInfo: $participantsInfo, ");
+    },
+    readyToClose: () {
+      debugPrint("readyToClose");
+    },
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _jitsiMeetPlugin.join(_jitsiMeetListener);
+  }
 
   @override
   Widget build(BuildContext context) {
